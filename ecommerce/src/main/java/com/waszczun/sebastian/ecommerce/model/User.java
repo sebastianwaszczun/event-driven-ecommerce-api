97876d491b819f02a1c6c8055ee76f5e350c4c03
Sebastian <xzunx@o2.pl>
2025-11-09 23:34:55 +0100

    Dodanie pol do encji Usera

--- ecommerce/src/main/java/com/waszczun/sebastian/ecommerce/model/User.java
+++ ecommerce/src/main/java/com/waszczun/sebastian/ecommerce/model/User.java
@@ -1,7 +1,13 @@
 package com.waszczun.sebastian.ecommerce.model;
 
 import jakarta.persistence.Entity;
+import lombok.RequiredArgsConstructor;
 
 @Entity
+@RequiredArgsConstructor
 public class User {
+
+    final String firstName;
+    final String lastName;
+    final int age;
 }
