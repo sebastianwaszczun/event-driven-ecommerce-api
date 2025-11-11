7c9bbeb9774f71c01a98f97fa20bb1b4795b51b8
Sebastian <xzunx@o2.pl>
2025-11-11 18:54:16 +0100

    Budowa UserService

--- ecommerce/src/main/java/com/waszczun/sebastian/ecommerce/service/UserService.java
+++ ecommerce/src/main/java/com/waszczun/sebastian/ecommerce/service/UserService.java
@@ -1,7 +1,42 @@
 package com.waszczun.sebastian.ecommerce.service;
 
+import com.waszczun.sebastian.ecommerce.model.User;
+import com.waszczun.sebastian.ecommerce.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
+    public User addUser(User user){
+        return userRepository.save(user);
+    }
+
+    public List<User> getUsers(){
+        return userRepository.findAll();
+    }
+
+    public void deleteUser(User user){
+        userRepository.delete(user);
+    }
+
+    public User editUser(User user){
+        Optional<User> byId = userRepository.findById(user.getId());
+        byId.get().setFirstName(user.getFirstName());
+        byId.get().setLastName(user.getLastName());
+        byId.get().setEmail(user.getEmail());
+        return byId.get();
+    }
+
+    public User userInfo(Long id){
+        Optional<User> byId = userRepository.findById(id);
+        return byId.get();
+    }
+
+
 }
