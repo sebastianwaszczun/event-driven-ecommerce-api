807d75bb1b5fb75c92103a605f56d4f7ecf1639f
Sebastian <xzunx@o2.pl>
2025-11-11 18:54:07 +0100

    Budowa UserController

--- ecommerce/src/main/java/com/waszczun/sebastian/ecommerce/controller/UserController.java
+++ ecommerce/src/main/java/com/waszczun/sebastian/ecommerce/controller/UserController.java
@@ -1,7 +1,45 @@
 package com.waszczun.sebastian.ecommerce.controller;
 
+import com.waszczun.sebastian.ecommerce.model.User;
+import com.waszczun.sebastian.ecommerce.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/user")
+    public User addUser(User user){
+        return userService.addUser(user);
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers(){
+        return userService.getUsers();
+    }
+
+    @DeleteMapping("/user")
+    public void deleteUser(User user){
+        userService.deleteUser(user);
+    }
+
+    @PutMapping("/user")
+    public User editUser(User user){
+        return userService.editUser(user);
+    }
+
+    @GetMapping("/user/{id}")
+    public User userInfo(Long id){
+        return userService.userInfo(id);
+    }
+
 }
