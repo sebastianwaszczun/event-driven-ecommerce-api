80d0f26b961bec59b1362a9ed4dc5b1ba88b22f5
Sebastian <xzunx@o2.pl>
2025-11-11 18:30:17 +0100

    Naprawa UserRepository

--- ecommerce/src/main/java/com/waszczun/sebastian/ecommerce/repository/UserRepository.java
+++ ecommerce/src/main/java/com/waszczun/sebastian/ecommerce/repository/UserRepository.java
@@ -2,9 +2,6 @@ package com.waszczun.sebastian.ecommerce.repository;
 
 import com.waszczun.sebastian.ecommerce.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-
-public class UserRepository extends JpaRepository<User, Long> {
-
+public interface UserRepository extends JpaRepository<User, Long> {
 }
