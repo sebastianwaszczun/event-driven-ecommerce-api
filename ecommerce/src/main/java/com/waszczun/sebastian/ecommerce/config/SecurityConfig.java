0b3cb3ba4c3331b1cdda2faf0286c85ffb289e64
Sebastian <xzunx@o2.pl>
2025-12-04 17:58:24 +0100

    Dodanie do autoryzacji - userow

--- ecommerce/src/main/java/com/waszczun/sebastian/ecommerce/config/SecurityConfig.java
+++ ecommerce/src/main/java/com/waszczun/sebastian/ecommerce/config/SecurityConfig.java
@@ -1,4 +1,18 @@
 package com.waszczun.sebastian.ecommerce.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(org.springframework.security.config.annotation.web.builders.HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // wyłącza CSRF
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); // wszystkie endpointy dostępne
+        return http.build();
+    }
 }
