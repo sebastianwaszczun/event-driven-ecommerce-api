4002ba3bff9026e2859d940ddc03e512eae2b394
Sebastian <xzunx@o2.pl>
2025-12-07 22:12:56 +0100

    Dodanie adnotacji do encji: @NoArgsConstructor @AllArgsConstructor Aby Hibernate działał

--- ecommerce/src/main/java/com/waszczun/sebastian/ecommerce/model/Product.java
+++ ecommerce/src/main/java/com/waszczun/sebastian/ecommerce/model/Product.java
@@ -4,7 +4,9 @@ package com.waszczun.sebastian.ecommerce.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
@@ -12,6 +14,8 @@ import java.math.BigDecimal;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
 
     @Id
