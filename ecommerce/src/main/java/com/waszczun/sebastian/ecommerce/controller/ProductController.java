6699614819e964f726457c738e1a4b83b7a4db63
Sebastian <xzunx@o2.pl>
2025-12-08 01:08:26 +0100

    Dodanie do ProductController getAllProducts i getProductById

--- ecommerce/src/main/java/com/waszczun/sebastian/ecommerce/controller/ProductController.java
+++ ecommerce/src/main/java/com/waszczun/sebastian/ecommerce/controller/ProductController.java
@@ -1,16 +1,27 @@
 package com.waszczun.sebastian.ecommerce.controller;
 
 import com.waszczun.sebastian.ecommerce.model.Product;
+import com.waszczun.sebastian.ecommerce.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.*;
+
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
+@RequiredArgsConstructor
 public class ProductController {
+    private final ProductService productService;
 
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return ResponseEntity.ok(productService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable int id) {
+        return ResponseEntity.ok(productService.findById(id));
+    }
 }
