d2e34e837f68a891acc41f3dd5b85b0d572bbf0b
Sebastian <xzunx@o2.pl>
2025-12-09 13:53:23 +0100

    Poprawa encji User

--- ecommerce/src/main/java/com/waszczun/sebastian/ecommerce/model/User.java
+++ ecommerce/src/main/java/com/waszczun/sebastian/ecommerce/model/User.java
@@ -1,9 +1,6 @@
 package com.waszczun.sebastian.ecommerce.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -17,7 +14,7 @@ import lombok.*;
 public class User {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @NotBlank(message = "First name is required")
