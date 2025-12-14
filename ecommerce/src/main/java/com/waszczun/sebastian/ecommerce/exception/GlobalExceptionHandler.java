44cf2fd4be3ba2bfea95d8326148610c40e0f03b
Sebastian <xzunx@o2.pl>
2025-12-15 00:09:22 +0100

    Dodanie globalnego wyjatku

---
+++ ecommerce/src/main/java/com/waszczun/sebastian/ecommerce/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.waszczun.sebastian.ecommerce.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleResourceNotFound(ResourceNotFoundException e) {
+        Map<String,String> errorResponse = new HashMap<>();
+        errorResponse.put("error", "Not Found");
+        errorResponse.put("message", e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+}
