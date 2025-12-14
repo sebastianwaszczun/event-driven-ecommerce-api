7c9827bb7995d5c397b4a722884bcac277e9b26c
Sebastian <xzunx@o2.pl>
2025-12-14 18:14:07 +0100

    Implementacja JwtTokenProvider

--- ecommerce/src/main/java/com/waszczun/sebastian/ecommerce/config/JwtTokenProvider.java
+++ ecommerce/src/main/java/com/waszczun/sebastian/ecommerce/config/JwtTokenProvider.java
@@ -1,4 +1,27 @@
 package com.waszczun.sebastian.ecommerce.config;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+import java.security.Key;
+import java.util.Date;
+
 public class JwtTokenProvider {
+    private static final String SECRET_KEY = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24)) // 24h
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
