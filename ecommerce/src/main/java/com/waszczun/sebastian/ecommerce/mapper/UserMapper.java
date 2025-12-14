f330d79a983120c8fccd8b05c129d82eec03f15b
Sebastian <xzunx@o2.pl>
2025-12-14 16:58:51 +0100

    Implementacja UserMapper

--- ecommerce/src/main/java/com/waszczun/sebastian/ecommerce/mapper/UserMapper.java
+++ ecommerce/src/main/java/com/waszczun/sebastian/ecommerce/mapper/UserMapper.java
@@ -1,4 +1,23 @@
 package com.waszczun.sebastian.ecommerce.mapper;
 
+import com.waszczun.sebastian.ecommerce.dto.UserRequest;
+import com.waszczun.sebastian.ecommerce.dto.UserResponse;
+import com.waszczun.sebastian.ecommerce.model.User;
+
 public class UserMapper {
+
+    public User toEntity(UserRequest userRequest) {
+        User user = new User();
+        user.setFirstName(userRequest.firstName());
+        user.setLastName(userRequest.lastName());
+        user.setPhoneNumber(userRequest.phoneNumber());
+        return user;
+    }
+
+    public UserResponse toResponse(User user) {
+        return new UserResponse(user.getId(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getPhoneNumber());
+    }
 }
