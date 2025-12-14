0bd3de2ede64bff65f12eedca25a41f61bebf4f8
Sebastian <xzunx@o2.pl>
2025-12-14 18:43:44 +0100

    Implementacja AuthController

--- ecommerce/src/main/java/com/waszczun/sebastian/ecommerce/controller/AuthController.java
+++ ecommerce/src/main/java/com/waszczun/sebastian/ecommerce/controller/AuthController.java
@@ -1,7 +1,24 @@
 package com.waszczun.sebastian.ecommerce.controller;
 
+import com.waszczun.sebastian.ecommerce.dto.AuthResponse;
+import com.waszczun.sebastian.ecommerce.dto.RegisterRequest;
+import com.waszczun.sebastian.ecommerce.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@Valid @RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
 }
