ae43f70660d1434f2af2d113746026f41d6dd613
Sebastian <xzunx@o2.pl>
2025-12-14 15:49:50 +0100

    Dodanie mapera dla Product

---
+++ ecommerce/src/main/java/com/waszczun/sebastian/ecommerce/mapper/ProductMapper.java
@@ -0,0 +1,32 @@
+package com.waszczun.sebastian.ecommerce.mapper;
+
+import com.waszczun.sebastian.ecommerce.dto.ProductRequest;
+import com.waszczun.sebastian.ecommerce.dto.ProductResponse;
+import com.waszczun.sebastian.ecommerce.model.Product;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductMapper {
+
+    //z DTO na encje - przy tworzeniu produktu
+    public Product toEntity(ProductRequest productRequest) {
+        Product product = new Product();
+        product.setName(productRequest.name());
+        product.setDescription(productRequest.description());
+        product.setPrice(productRequest.price());
+        product.setStockQuantity(productRequest.stockQuantity());
+        return product;
+    }
+
+    //z encji do DTO - przy wyswietlaniu
+    public ProductResponse toResponse(Product product) {
+        return new ProductResponse(
+                product.getId(),
+                product.getName(),
+                product.getDescription(),
+                product.getCategory(),
+                product.getPrice(),
+                product.getStockQuantity()
+        );
+    }
+}
